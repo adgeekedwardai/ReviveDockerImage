fa0a553ed0e10679bb775b3201c9b03fe9ffc705
adgeekedwardai <edward.ai@adgeek.com.tw>
2021-07-05 03:48:42 +0000

    [init] code

---
+++ code/var/default.conf.php
@@ -0,0 +1,3 @@
+;<?php exit; ?>
+;*** DO NOT REMOVE THE LINE ABOVE ***
+realConfig="8080-magenta-hornet-qp9pos3p.ws-us10.gitpod.io"
---
+++ code/var/templates_compiled/%%8D^8D7^8D794F90%%content-footer.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2021-07-05 03:44:48
+         compiled from layout/content-footer.html */ ?>
+<?php if ($this->_tpl_vars['showContentFrame']): ?>
+</div>
+<?php endif; ?>
